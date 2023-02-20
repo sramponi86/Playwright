782d1dadf2edf354307ce912039ddcdd72f213f1
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2023-02-20 11:43:27 +0100

    Add files via upload

--- cucumber/cucumber.js
+++ cucumber/cucumber.js
@@ -1,8 +1,23 @@
-const common = `
-    --require setup/assertions.js
-    --require setup/hooks.js
-    --require step-defitions/**/*.step.js
-`
+//const common = `
+ //   --require setup/assertions.js
+  //  --require setup/hooks.js
+   // --require step-defitions/**/*.step.js
+//`
+//module.exports = {
+ // default: `${common} features/**/*.feature`,
+//}
+
+let options = [
+  '--require setup/assertions.js',
+    '--require setup/hooks.js',
+    '--require step-defitions/**/*.step.js',
+].join(' ');
+
+let run_features = [
+  './features/',
+  options,
+].join(' ');
+
 module.exports = {
-  default: `${common} features/**/*.feature`,
-}
+  test_runner: run_features
+};
